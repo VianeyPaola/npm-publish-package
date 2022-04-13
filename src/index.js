58edd7649a31f40c60d89740d5606b99caada270
VianeyPaola <paaolaaherrera@gmail.com>
2022-04-13 00:07:07 -0500

    paquete npm random message

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Se declara el arreglo
+const messages = [
+    "David",
+    "Diana",
+    "Ana Maria",
+    "Isabela",
+    "Antonio",
+    "Norma",
+    "Paola",
+    "Fanny",
+    "Cochi"
+]
+
+//Crear función para enviar aleatoriamente  los nombres del arreglo
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)]
+    console.log(message)
+}
+
+// Exportar como un módulo
+
+module.exports = { randomMsg }
